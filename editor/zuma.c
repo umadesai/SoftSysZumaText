fc842e84ffecea7fc1d56bbc26f80c99d0254b96
Uma Desai <uma.desai@students.olin.edu>
2018-02-27 00:08:23 -0500

    dealt with flags, turned off output processing, added timeout, started error handling

--- editor/zuma.c
+++ editor/zuma.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <errno.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,34 +7,52 @@
 
 struct termios orig_termios;
 
-// turn off echoing and canonical mode
-void enableRawMode(){
-  struct termios raw;
-
-  tcgetattr(STDIN_FILENO, &raw);
-
-  raw.c_lflag &= ~(ECHO | ICANON);
-
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+void die(const char *s) {
+  // prints error message and exits the program
+  perror(s);
+  exit(1);
 }
 
 // disable raw mode at exit
 void disableRawMode(){
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+// turn off echoing and canonical mode
+void enableRawMode(){
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+
+  // fix Ctrl-M and disable Ctrl-S, Ctrl-Q, and other miscellaneous flags
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  // turn off all output processing
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  // disable Ctrl-C, Ctrl-Z, and Ctrl-V
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 
 int main(){
   enableRawMode();
 
-  char c;
-  // display keypresses
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
+
   return 0;
 }
