831750206ebe681046ed862234d612f94d64e0d9
Uma Desai <uma.desai@students.olin.edu>
2018-03-11 21:59:45 -0400

    updated zuma.c

--- editor/zuma.c
+++ editor/zuma.c
@@ -13,7 +13,17 @@
 
 #define DRAW_TILDES(str, n_bytes) write(STDOUT_FILENO, str, (n_bytes))
 
+#define ZUMA_VERSION "0.0.1"
+
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
+
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -89,26 +99,71 @@ void abFree(struct abuf *ab) {
 }
 
 // prompt for key
-char editorReadKey() {
+int editorReadKey() {
   int n_read;
   char c;
-  while ((n_read = read(STDIN_FILENO, &c, 1)) != 1)
+  while ((n_read = read(STDIN_FILENO, &c, 1)) != 1){
     if (n_read == -1 && errno != EAGAIN) die("editorReadKey");
-  return c;
+  }
+
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
+      }
+    }
+
+    return '\x1b';
+  } else {
+    return c;
+  }
+}
+
+void editorMoveCursor(int key) {
+  switch (key) {
+    case ARROW_LEFT:
+      conf.cx--;
+      break;
+    case ARROW_RIGHT:
+      conf.cx++;
+      break;
+    case ARROW_UP:
+      conf.cy--;
+      break;
+    case ARROW_DOWN:
+      conf.cy++;
+      break;
+  }
 }
 
 // prompt for signal processing
 int editorProcessKeyPress(){
-  char c = editorReadKey();
+  int c = editorReadKey();
   switch (c) {
     case CTRL_KEY('q'):
       editorClearScreen();
       return -1;
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+      editorMoveCursor(c);
+      break;
   }
   return 0;
 }
 
 void initEditor() {
+  conf.cx = 0;
+  conf.cy = 0;
   if (getWindowSize(&conf.screenrows, &conf.screencols) == -1) die("getWindowSize");
 }
 
@@ -117,16 +172,51 @@ void editorClearScreen(){
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
-void editorRefreshScreen(){
-  editorClearScreen();
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < conf.screenrows; y++) {
-    DRAW_TILDES("~", 1);
+    if (y == conf.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", ZUMA_VERSION);
+      if (welcomelen > conf.screencols) welcomelen = conf.screencols;
+      int padding = (conf.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+    // clear lines one at a time
+    abAppend(ab, "\x1b[K", 3);
     if (y < conf.screenrows - 1) {
-      DRAW_TILDES("\r\n", 2);
+      abAppend(ab, "\r\n", 2);
     }
   }
-  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorRefreshScreen(){
+  struct abuf ab = ABUF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
+
+  editorDrawRows(&ab);
+
+  // move the cursor to .cx,.cy position
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", conf.cy + 1, conf.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
+  abAppend(&ab, "\x1b[H", 3);
+  // hide the cursor when repainting
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 int main(){
