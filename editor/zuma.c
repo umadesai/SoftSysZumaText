572600ed64e9a1d1ae3b55c8999630339b10a2e6
Zhengyang Feng <zhengyang.feng@students.olin.edu>
2018-03-15 06:50:37 -0400

    raw input and output

--- editor/zuma.c
+++ editor/zuma.c
@@ -20,6 +20,9 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -30,6 +33,11 @@ struct editorConfig {
   int screencols;
   struct termios orig_termios;
 } conf;
+
+void editorClearScreen(){
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
 
 void die(const char *s) {
   editorClearScreen();
@@ -115,14 +123,35 @@ int editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
     if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '1': return HOME_KEY; // esc [1~
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP; // esc [A
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
       switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+        case 'H': return HOME_KEY;  // esc 0H
+        case 'F': return END_KEY;
       }
     }
-
     return '\x1b';
   } else {
     return c;
@@ -153,11 +182,27 @@ int editorProcessKeyPress(){
     case CTRL_KEY('q'):
       editorClearScreen();
       return -1;
+
+    case HOME_KEY:
+      conf.cx = 0;
+      break;
+    case END_KEY:
+      conf.cx = conf.screencols - 1;
+      break;
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(c);
+      break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+      int times = conf.screenrows;
+      while (times--) editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
       break;
   }
   return 0;
@@ -169,10 +214,7 @@ void initEditor() {
   if (getWindowSize(&conf.screenrows, &conf.screencols) == -1) die("getWindowSize");
 }
 
-void editorClearScreen(){
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
-}
+
 
 void editorDrawRows(struct abuf *ab) {
   int y;
